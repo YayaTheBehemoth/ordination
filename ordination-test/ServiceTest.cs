bf8cda109e8e7ee6329f42e10baf8e46ae80e911
Wxlfgang <oellegaardwilliam@gmail.com>
2022-12-01 12:06:18 +0100

    dagligfasttest og dagligskævtest done

--- ordination-test/ServiceTest.cs
+++ ordination-test/ServiceTest.cs
@@ -39,4 +39,37 @@ public class ServiceTest
 
         Assert.AreEqual(5, service.GetPNs().Count());
     }
+
+    [TestMethod]
+    public void OpretDagligFast()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+
+        Assert.AreEqual(1, service.GetDagligFaste().Count());
+
+        service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId, 2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligFaste().Count());
+        Assert.AreNotEqual(3, service.GetDagligFaste().Count());
+
+    }
+    [TestMethod]
+    public void OpretDagligSkaev()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        Dosis[] doser = new Dosis[]
+        {
+            new Dosis (CreateTimeOnly(12, 0, 0), 1),
+            new Dosis (CreateTimeOnly(16, 30, 0), 4),
+            new Dosis (CreateTimeOnly(22, 45, 0), 2)
+        };
+        Assert.AreEqual(1, service.GetDagligSkæve().Count());
+        service.OpretDagligSkaev(patient.PatientId, lm.LaegemiddelId, doser, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligSkæve().Count());
+
+
+    } 
 }
